d427a1f76ba18a7d623c2fcdf7e1ae3bb9d27386
aSanchezzz <sanchez.reyes2501@gmail.com>
2017-12-06 11:20:51 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+
+//COMPONENTS
+
+
+class App extends Component {
+  
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
